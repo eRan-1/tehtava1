ac3770a528595829c39d41cc3d2b77f08bb2e2a3
eRan-1 <mika.erander@studen.lut.fi>
2024-06-03 15:47:06 +0300

    tehtava1

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,9 +1,9 @@
 package main;
-
+//Mika Erander, weekly assignment 1. App.java
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" ); //display the string
     }
 }
